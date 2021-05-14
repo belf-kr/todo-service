c06522df5b62667f26cc688453acc2a3dd8e089d
Kyungeun <ruddms936@naver.com>
2021-05-14 11:16:32 +0900

    add

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 export interface HelloRes {
   value1: string;
+  value2: string;
 }
 
 @Injectable()
@@ -9,6 +10,7 @@ export class AppService {
   getHello(): HelloRes {
     return {
       value1: "todo-service 1",
+      value2: "todo-service 2",
     };
   }
 }
