4a742e379db1313eb1de21ed8525a03146031a20
Kyungeun <ruddms936@naver.com>
2021-05-14 10:18:43 +0900

    add: 테스트를 위한 res

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
 
+export interface HelloRes {
+  value1: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  getHello(): HelloRes {
+    return {
+      value1: "todo-service 1",
+    };
   }
 }
