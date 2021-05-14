00ab2d108ec9bab04a90800c4e7e8b9a0a21b849
Kyungeun <ruddms936@naver.com>
2021-05-14 11:54:21 +0900

    add: test를 위해 삭제

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 
 export interface HelloRes {
   value1: string;
-  value2: string;
 }
 
 @Injectable()
@@ -10,7 +9,6 @@ export class AppService {
   getHello(): HelloRes {
     return {
       value1: "todo-service 1",
-      value2: "todo-service 2",
     };
   }
 }
