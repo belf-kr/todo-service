f55660565422065f484f50b02e5f736d95fc1944
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-16 17:27:15 +0900

    add: Course_tag 테이블 생성 위한 ORM 코드 추가
    closes #19

---
+++ src/entity/course-tag.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Course } from "./course.entity";
+import { Tag } from "./tag.entity";
+
+@Entity({})
+export class CourseTag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Course, (course) => course.id, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "course_id" })
+  courseId: number;
+
+  @ManyToOne(() => Tag, (tag) => tag.id, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "tag_id" })
+  tagId: number;
+}
