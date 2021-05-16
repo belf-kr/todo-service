490606bbb3bc96c30c7804196f87a2bda17bfab9
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-16 17:05:02 +0900

    add: Repeated_days_of_the_week) 테이블 생성위한 ORM 코드 추가
    closes #19

---
+++ src/entity/repeated-day-of-the-week.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { WorkTodo } from "./work-todo.entity";
+
+@Entity({})
+export class RepeatedDaysOfTheWeek {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => WorkTodo, (work_to_do) => work_to_do.id, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "work_todo_id" })
+  workToDoId: number;
+
+  @Column({
+    name: "day_of_the_week",
+  })
+  dayOfTheWeek: number;
+}
