e8f12c8bfe516e93bb4f3ed9dc09f7547888c3ce
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-18 14:37:45 +0900

    up: 사용되지 않은 import 구문 삭제

--- src/entity/repeated-day-of-the-week.entity.ts
+++ src/entity/repeated-day-of-the-week.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { WorkTodo } from "./work-todo.entity";
 
 @Entity({})
