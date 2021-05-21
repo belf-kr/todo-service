24339b616619632b222dd3c28375d5d23297d975
Seungup Noh <su.noh@kakao.com>
2021-05-21 23:32:17 +0900

    update 기본 세팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService, HelloRes } from "./app.service";
+import { Controller, Get, HttpStatus, Res } from "@nestjs/common";
+import { Response } from "express";
+import { AppService } from "./app.service";
+import { version } from "../package.json";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): HelloRes {
-    return this.appService.getHello();
+  @Get("ping")
+  ping(@Res() res: Response) {
+    res.sendStatus(HttpStatus.OK);
+  }
+
+  @Get("version")
+  version() {
+    return version;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+let APP_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  init();
+  await app.listen(APP_PORT);
 }
 bootstrap();
+
+function init() {
+  if (!process.env.STAGES) {
+    process.env.STAGES = "LOCAL";
+    APP_PORT = 3002;
+  }
+}
