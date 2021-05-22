9f7db63325f0a1ab09a668dee8c492a0bd77319f
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-22 22:03:29 +0900

    add: .env 파일을 사용해 DB 연결정보 입력받는 기능 추가
    closes #28

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TagModule } from "./tag/tag.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TagModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: `.env`
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: "mysql",
+        host: process.env.DB_MASTER_HOST,
+        port: parseInt(process.env.DB_MASTER_PORT),
+        username: process.env.DB_MASTER_USERNAME,
+        password: process.env.DB_MASTER_PASSWORD,
+        database: process.env.DB_MASTER_DATABASE,
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: true,
+      }),
+    }),
+    TagModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
