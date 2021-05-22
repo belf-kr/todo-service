bbbbea0c4e48a6f2f4eace0d4729f8b987270e71
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-22 14:36:17 +0900

    add: Read 기능 추가
    closes #18

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Tag } from "src/entity/tag.entity";
+import { TagService } from "./tag.service";
 
 @Controller("tag")
-export class TagController {}
+export class TagController {
+  constructor(private readonly tagService: TagService) {
+    this.tagService = tagService;
+  }
+  @Get()
+  async findAll(): Promise<Tag[]> {
+    const tagList = await this.tagService.findAll();
+
+    return Object.assign({
+      data: tagList,
+      statusCode: 200,
+      statusMsg: `데이터 조회가 성공적으로 완료되었습니다.`,
+    });
+  }
+
+  @Get(":tagId")
+  async find(@Param("tagId") tagId: string): Promise<Tag> {
+    const tag = await this.tagService.findOne(tagId);
+
+    return Object.assign({
+      data: tag,
+      statusCode: 200,
+      statusMsg: `데이터 조회가 성공적으로 완료되었습니다.`,
+    });
+  }
+}
