f141e1a97e81c9c608c7623cc6105e14e51cd9d4
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-22 22:34:43 +0900

    up: .env 파일 내 DB 연결 관련 내용 없을 경우 위한 기본값 추가
    closes #28

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,10 @@
 module.exports = {
   type: "mysql",
-  host: process.env.DB_MASTER_HOST,
-  port: parseInt(process.env.DB_MASTER_PORT),
-  username: process.env.DB_MASTER_USERNAME,
-  password: process.env.DB_MASTER_PASSWORD,
-  database: process.env.DB_MASTER_DATABASE,
+  host: process.env.DB_MASTER_HOST || "localhost",
+  port: parseInt(process.env.DB_MASTER_PORT) || 3306,
+  username: process.env.DB_MASTER_USERNAME || "root",
+  password: process.env.DB_MASTER_PASSWORD || "example",
+  database: process.env.DB_MASTER_DATABASE || "belf",
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: true,
 };
