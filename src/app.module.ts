26846433f9a2e1d5dfeab5f84656bdd1de93956b
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-22 14:07:42 +0900

    add: CRUD 확인 위한 Tag 모듈 추가
    closes #18

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
