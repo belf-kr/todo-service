7099a5c8b382525c122374849b73b23fdd4b2115
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-23 16:49:04 +0900

    add: TypeORM migration npm 사용 가능하게 스크립트 추가
    closes #28 #32

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,8 @@
+import { ConnectionOptions } from "typeorm";
+import DbConfig from "./database.config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const typeormConfig = DbConfig() as ConnectionOptions;
+
+export default typeormConfig;
