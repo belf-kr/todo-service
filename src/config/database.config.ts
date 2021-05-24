e643db67bba3e8793df44d05da161ba037cb8d27
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-24 17:54:43 +0900

    add: TypeORM Master, Slave 설정 추가
    closes #16

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,16 +1,29 @@
 const DatabaseConfig = () => ({
   type: "mysql",
-  host: process.env.DB_MASTER_HOST || "localhost",
-  port: parseInt(process.env.DB_MASTER_PORT) || 3306,
-  username: process.env.DB_MASTER_USERNAME || "root",
-  password: process.env.DB_MASTER_PASSWORD || "example",
-  database: process.env.DB_MASTER_DATABASE || "belf",
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: process.env.DB_SYNCHRONIZE || false,
   migrationsTableName: "migrations",
   migrations: ["dist/src/migrations/*{.ts,.js}"],
   cli: {
     migrationsDir: "src/migrations",
+  },
+  replication: {
+    master: {
+      host: process.env.DB_MASTER_HOST || "localhost",
+      port: parseInt(process.env.DB_MASTER_PORT) || 3306,
+      username: process.env.DB_MASTER_USERNAME || "root",
+      password: process.env.DB_MASTER_PASSWORD || "example",
+      database: process.env.DB_MASTER_DATABASE || "belf",
+    },
+    slaves: [
+      {
+        host: process.env.DB_SLAVE_HOST || "localhost",
+        port: parseInt(process.env.DB_SLAVE_PORT) || 3307,
+        username: process.env.DB_SLAVE_USERNAME || "root",
+        password: process.env.DB_SLAVE_PASSWORD || "example",
+        database: process.env.DB_SLAVE_DATABASE || "belf",
+      },
+    ],
   },
 });
 
