8ce7e77b7b26fff153bb122558e6b7c0c7a83b08
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-28 20:20:49 +0900

    add: PK만 존재하는 테이블에 질의를 날리는 관련 파일 생성
    closes #39

--- src/example-upper/example-upper.module.ts
+++ src/example-upper/example-upper.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ExampleUpper } from "src/entity/example-upper.entity";
 import { ExampleUpperController } from "./example-upper.controller";
 import { ExampleUpperService } from "./example-upper.service";
 
 @Module({
+  // Typeorm.forFeature 메소드를 사용해 사용할 엔테테를 인식 해 주지 않으면 에러가 발생한다.
+  imports: [TypeOrmModule.forFeature([ExampleUpper])],
   controllers: [ExampleUpperController],
   providers: [ExampleUpperService],
 })
--- src/example-upper/example-upper.service.ts
+++ src/example-upper/example-upper.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExampleUpper } from "src/entity/example-upper.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ExampleUpperService {}
+export class ExampleUpperService {
+  // DB와 연관이 있는 서비스를 사용할 때 DB 접속에 사용될 레포지토리를 생성한다.
+  constructor(
+    // Repository<ExampleUpper>의 의존성이 주입 된다는 것을 의미한다.
+    // DB에서 CRUD를 실행할 때 각 메소드에서 사용한다.
+    @InjectRepository(ExampleUpper)
+    private exampleUpperRepository: Repository<ExampleUpper>
+  ) {}
+}
