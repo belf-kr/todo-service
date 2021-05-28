7ca29c1258ffd4031338f46bce0e0d66150c3f5a
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-28 19:24:32 +0900

    add: 외래키를 사용하지 않는 예제 테이블 생성
    closes #39

---
+++ src/entity/example-upper.entity.ts
@@ -0,0 +1,49 @@
+import { Column, CreateDateColumn, Double, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({})
+export class ExampleUpper {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({
+    type: "datetime",
+    name: "creation_date",
+    comment: "행이 생성될 때 해당 날짜와 시간을 자동 입력합니다.",
+  })
+  creationDate: Date;
+
+  @UpdateDateColumn({
+    type: "datetime",
+    name: "update_date",
+    comment: "행이 업데이트 될 때 마다 해당 날짜와 시간을 자동 입력합니다.",
+  })
+  updateDate: Date;
+
+  @Column({
+    length: 100,
+    type: "nvarchar",
+    charset: "utf8mb4",
+  })
+  title: string;
+
+  @Column({
+    type: "text",
+    charset: "utf8",
+    collation: "utf8_unicode_ci",
+    comment: "특정 컬럼의 charset, collection을 변경 할 수 있는 방법을 보여주기 위한 예제입니다.",
+  })
+  content: string;
+
+  @Column({
+    type: "double",
+    default: 50.0,
+  })
+  defaultDouble: Double;
+
+  @Column({
+    type: "int",
+    nullable: true,
+    comment: "null값이 허용되는 컬럼입니다.",
+  })
+  nullableInt: number;
+}
