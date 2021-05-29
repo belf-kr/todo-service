ac64e5692c92fe7fb2fc9081c1095290dfb8d879
JP3Pe <rlaworhkd430430@gmail.com>
2021-05-29 12:02:16 +0900

    add: 단일 1행을 Update 하는 관련 코드 추가
    closes #39

--- src/example-upper/example-upper.controller.ts
+++ src/example-upper/example-upper.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Post, Put } from "@nestjs/common";
 import { HttpException } from "@nestjs/common";
-import { exception } from "console";
 
 import { ExampleUpperService } from "./example-upper.service";
 
@@ -32,24 +31,40 @@ export class ExampleUpperController {
     }
   }
 
-  //   1개 행 Read
+  // 1개 행 Read
   @Get()
   async read(@Body() exampleUpper: ExampleUpper): Promise<HttpStatus> {
     try {
       const result = await this.exampleUpperService.read(exampleUpper);
 
       // Service가 동작 된 경우
-      if (result !== undefined) {
-        return Object.assign({
-          status: HttpStatus.CREATED,
-          msg: `read successfully`,
-          data: { ...result },
-        });
-      }
+      return Object.assign({
+        status: HttpStatus.CREATED,
+        msg: `read successfully`,
+        data: { ...result },
+      });
       // 동작에 실패한 경우 Catch 구문에 예외를 넘김
-      else {
-        throw exception;
-      }
+    } catch {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: `read failed`,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  // 1개 행 Update
+  @Put()
+  async update(@Body() exampleUpper: ExampleUpper): Promise<HttpStatus> {
+    try {
+      await this.exampleUpperService.update(exampleUpper);
+
+      return Object.assign({
+        status: HttpStatus.OK,
+        msg: `update successfully`,
+      });
     } catch {
       throw new HttpException(
         {
