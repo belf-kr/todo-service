fa2ba1fcdbb1c7506696a13da3aca53ce8434fe4
박경은 <ruddms936@naver.com>
2021-06-05 21:14:38 +0900

    up: 명확한 키워드로 변경

--- src/example-lower/example-lower.controller.ts
+++ src/example-lower/example-lower.controller.ts
@@ -10,7 +10,7 @@ import { CRUDController } from "src/common/crud.controller";
 // 어떤 타입을 컨트롤러에서 종적으로 사용하고 싶은지 <자료형> 식으로 입력
 export class ExampleLowerController extends CRUDController<ExampleLower> {
   // 서비스 객체를 생성 후, 부모 클래스의 생성자에 super 키워드를 사용해 넣어 줌
-  constructor(readonly exampleLowerService: ExampleLowerService) {
+  constructor(private readonly exampleLowerService: ExampleLowerService) {
     super(exampleLowerService);
   }
 }
--- src/example-upper/example-upper.controller.ts
+++ src/example-upper/example-upper.controller.ts
@@ -10,7 +10,7 @@ import { CRUDController } from "src/common/crud.controller";
 // 어떤 타입을 컨트롤러에서 종적으로 사용하고 싶은지 <자료형> 식으로 입력
 export class ExampleUpperController extends CRUDController<ExampleUpper> {
   // 서비스 객체를 생성 후, 부모 클래스의 생성자에 super 키워드를 사용해 넣어
-  constructor(readonly exampleUpperService: ExampleUpperService) {
+  constructor(private readonly exampleUpperService: ExampleUpperService) {
     super(exampleUpperService);
   }
 }
