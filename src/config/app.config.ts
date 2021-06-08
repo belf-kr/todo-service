1a72d72cf34f4e6f8a43fdd2d7eacb1dffd3b2ac
JP3Pe <rlaworhkd430430@gmail.com>
2021-06-08 16:46:05 +0900

    del: Listen 포트 번호를 중복해 선언하는 구문 삭제
    app.config.ts 내 OR 절을 주지 않아도 Joi 에서 기본값을 줄 수 있는 것을 확인
    
    closes #56

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,7 @@
 import DatabaseConfig from "./database.config";
 
 export default () => ({
-  port: parseInt(process.env.SERVER_PORT) || 3000,
+  port: parseInt(process.env.SERVER_PORT),
   database: {
     ...DatabaseConfig(),
   },
