4cdabcdfa676267534478514b0a2a994f4ae1b2b
JP3Pe <rlaworhkd430430@gmail.com>
2021-06-12 01:24:34 +0900

    add: course-tag, tag 관련 CRUD 상속
    closes #63

--- src/course-tag/course-tag.module.ts
+++ src/course-tag/course-tag.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { CourseTagService } from "./course-tag.service";
 import { CourseTagController } from "./course-tag.controller";
 
+import { CourseTag } from "src/entity/course-tag.entity";
+
 @Module({
+  imports: [TypeOrmModule.forFeature([CourseTag])],
   providers: [CourseTagService],
   controllers: [CourseTagController],
 })
--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { TagService } from "./tag.service";
 import { TagController } from "./tag.controller";
 
+import { Tag } from "src/entity/tag.entity";
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
   providers: [TagService],
   controllers: [TagController],
 })
