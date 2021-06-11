68ec9376b3d6ffead0767f606a857df25f74b50e
JP3Pe <rlaworhkd430430@gmail.com>
2021-06-12 00:05:54 +0900

    up: 외래키 없는 코스 생성 로직 상속받지 않게 수정
    차후 외래키에 대한 처리를 위함
    
    closes #63

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,6 +1,7 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpException, HttpStatus, Post } from "@nestjs/common";
 
 import { CRUDController } from "src/common/crud.controller";
+import { getErrorHttpStatusCode, getErrorMessage } from "src/common/lib/error";
 import { Course } from "src/entity/course.entity";
 
 import { CourseService } from "./course.service";
@@ -9,5 +10,21 @@ import { CourseService } from "./course.service";
 export class CourseController extends CRUDController<Course> {
   constructor(private readonly courseService: CourseService) {
     super(courseService);
+  }
+
+  @Post("create-course")
+  async createCourse(@Body() courseEntity: Course[]): Promise<void> {
+    try {
+      await this.courseService.createCourse(courseEntity);
+
+      return Object.assign({
+        status: HttpStatus.CREATED,
+        msg: `create successfully`,
+      });
+    } catch (error) {
+      const httpStatusCode = getErrorHttpStatusCode(error);
+      const message = getErrorMessage(error);
+      throw new HttpException(message, httpStatusCode);
+    }
   }
 }
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Post } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
 import { CRUDService } from "src/common/crud.service";
 import { Course } from "src/entity/course.entity";
-import { Repository } from "typeorm";
 
 @Injectable()
 export class CourseService extends CRUDService<Course> {
   constructor(@InjectRepository(Course) courseRepository: Repository<Course>) {
     super(courseRepository);
+  }
+
+  @Post("create-course")
+  async createCourse(courseEntity: Course[]): Promise<void> {
+    return this.create(courseEntity);
   }
 }
