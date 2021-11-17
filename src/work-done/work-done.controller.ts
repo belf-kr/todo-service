1ea32b8fdc09b9b5eef549d3f15902694f5aed72
JP3Pe <rlaworhkd430430@gmail.com>
2021-11-17 20:04:12 +0900

    up: Work-done 조회시 userId 이용한 필터기능 추가

--- src/work-done/work-done.controller.ts
+++ src/work-done/work-done.controller.ts
@@ -27,11 +27,11 @@ export class WorkDoneController {
 
   // TODO: Custom pipe 만들어 param을 선택 사항 가능하게 만들기
   @Get()
-  async getWorkTodosByConditions(@Query("courseId") courseId?: number) {
+  async getWorkTodosByConditions(@Query("userId") userId: number, @Query("courseId") courseId?: number) {
     let serviceResult: WorkDoneDto[];
 
     try {
-      const querystringInput = new WorkDoneQuerystringDto(courseId);
+      const querystringInput = new WorkDoneQuerystringDto(userId, courseId);
 
       // 할일 리스트 저장
       serviceResult = await this.workDoneService.getWorkDonesByConditions(querystringInput);
--- src/work-done/work-done.service.ts
+++ src/work-done/work-done.service.ts
@@ -57,12 +57,16 @@ export class WorkDoneService extends CRUDService<WorkDone> {
     /*
       FROM    work_done AS wd
     */
-    let queryString = getRepository(WorkDone).createQueryBuilder("wd");
+    let sqlQueryString = getRepository(WorkDone).createQueryBuilder("wd");
 
     if (querystringInput?.courseId) {
-      const workTodoQuerystringInput = new WorkTodoQuerystringDto(querystringInput.courseId);
+      const workTodoQuerystringInput = new WorkTodoQuerystringDto(querystringInput.userId, querystringInput.courseId);
       // courseId 값을 가지고 있는 WorkDoneDto 객체의 배열을 생성한다.
       const wokrTodoDtoArrayResult = await this.workTodoService.getWorkTodosByConditions(workTodoQuerystringInput);
+      if (wokrTodoDtoArrayResult.length === 0) {
+        throw new HttpException({ data: "workTodo의 id 값을 만족하는 데이터가 없습니다.", status: HttpStatus.BAD_REQUEST }, HttpStatus.BAD_REQUEST);
+      }
+
       const workTodoIdArray = new Array<number>();
       // number 배열을 workTodoDtoArrayResult로 부터 생성한다.
       for (const item of wokrTodoDtoArrayResult) {
@@ -74,10 +78,11 @@ export class WorkDoneService extends CRUDService<WorkDone> {
                 INNER JOIN course c on wt.course_id = c.id
         WHERE   wd.work_todo_id IN (?);
       */
-      queryString = queryString
+      sqlQueryString = sqlQueryString
         .innerJoinAndMapMany("wd", WorkTodo, "wt", "wd.work_todo_id = wt.id")
         .innerJoinAndMapMany("wt", Course, "c", "wt.course_id = c.id")
-        .where("wd.work_todo_id in (:workTodoIds)", { workTodoIds: workTodoIdArray });
+        .where("wd.user_id = :userId", { userId: querystringInput.userId })
+        .andWhere("wd.work_todo_id in (:workTodoIds)", { workTodoIds: workTodoIdArray });
     }
 
     // query string 을 사용해 SELECT 수행
@@ -89,7 +94,7 @@ export class WorkDoneService extends CRUDService<WorkDone> {
                 wd.action_date,
                 wd.work_todo_id
       */
-    const selectResult = await queryString.getRawMany();
+    const selectResult = await sqlQueryString.getRawMany();
 
     // SELECT 결과 DTO 배열에 매핑
     for (const selectItem of selectResult) {
