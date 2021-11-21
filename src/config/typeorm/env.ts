a3fb43eadcb3e87f50a5f2bb499cabc0e24b9578
JP3Pe <rlaworhkd430430@gmail.com>
2021-11-21 20:20:40 +0900

    up: 로그 추가

--- src/config/typeorm/env.ts
+++ src/config/typeorm/env.ts
@@ -1,6 +1,8 @@
 import dotenv = require("dotenv");
 import path = require("path");
 
+console.log("Debug: This is env.ts file imported context");
 dotenv.config({
   path: path.resolve(process.cwd(), process.env.NODE_ENV == "development" ? ".env.dev" : ""),
 });
+console.log("Debug end: env.ts finished");
