921fe9fb5903f96ddbfda716fd2bdb696d752d1a
JP3Pe <rlaworhkd430430@gmail.com>
2021-11-26 11:21:53 +0900

    add: 현재 ms 표시기능 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,7 +13,7 @@ export class AppController {
 
   @Get("ping")
   getPing(): void {
-    console.log(`Begin controller: ${new Date().toUTCString}`);
+    console.log(`Begin controller: ${new Date().toUTCString()}, and ${new Date().getUTCMilliseconds()}ms`);
     return;
   }
 
